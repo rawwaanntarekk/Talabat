ace9315c27363da6fec8b2a3da13f355bb005a45
Rawan Tarek <115898539+rawwaanntarekk@users.noreply.github.com>
2024-10-05 14:05:52 +0300

    Part 03 Product Module - Entities.

---
+++ LinkDev.Talabat.Domain/Products/Product.cs
@@ -0,0 +1,21 @@
+﻿using LinkDev.Talabat.Core.Domain.Common;
+
+namespace LinkDev.Talabat.Core.Domain.Products
+{
+    public class Product : BaseEntity<int>
+    {
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public string? PictureUrl { get; set; }
+        public decimal Price { get; set; }
+        public int? BrandId { get; set; } // Forign key 
+        public virtual ProductBrand? Brand { get; set; }
+        public int? MyProperty { get; set; } // Forign key
+        public virtual ProductCategory? Category { get; set; }
+
+
+
+
+
+    }
+}
