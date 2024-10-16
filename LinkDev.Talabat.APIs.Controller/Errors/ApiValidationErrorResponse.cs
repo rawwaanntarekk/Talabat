c3cfde6d65582b96c9f85642238c0bc4e4c43549
Rawan Tarek <115898539+rawwaanntarekk@users.noreply.github.com>
2024-10-16 06:59:42 +0300

    Part 03 Validation Error Handling.

---
+++ LinkDev.Talabat.APIs.Controller/Errors/ApiValidationErrorResponse.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.APIs.Controllers.Errors
+{
+    public class ApiValidationErrorResponse : ApiResponse
+    {
+        public ApiValidationErrorResponse(string? message = null) : base(400)
+        {
+        }
+        public required IEnumerable<string> Errors { get; set; }
+       
+    }
+}
--- LinkDev.Talabat.APIs/Program.cs
+++ LinkDev.Talabat.APIs/Program.cs
@@ -4,6 +4,8 @@ using LinkDev.Talabat.Core.Application.Abstraction.Contracts;
 using LinkDev.Talabat.Infrastructure.Persistence;
 using LinkDev.Talabat.Core.Application;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using LinkDev.Talabat.APIs.Controllers.Errors;
 namespace LinkDev.Talabat.APIs
 {
 	public class Program
@@ -16,7 +18,23 @@ namespace LinkDev.Talabat.APIs
             // Add services to the container.
 
             webApplicationBuilder.Services.AddControllers()
-                                          .AddApplicationPart(typeof(Controllers.AssemblyInformation).Assembly);
+                                          .AddApplicationPart(typeof(Controllers.AssemblyInformation).Assembly)
+                                          .ConfigureApiBehaviorOptions(options =>
+                                          {
+                                              options.SuppressModelStateInvalidFilter = false;
+                                              options.InvalidModelStateResponseFactory = (actionContext) =>
+                                              {
+                                                  var errors = actionContext.ModelState.Where(P => P.Value!.Errors.Count > 0)
+                                                                         .SelectMany(P => P.Value!.Errors)
+                                                                         .Select(E => E.ErrorMessage);
+
+                                                  return new BadRequestObjectResult(new ApiValidationErrorResponse()
+                                                  {
+                                                      Errors = errors
+                                                  });
+                                              };
+
+                                          });
             // Register Required Services for Controllers in the DI Container
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
