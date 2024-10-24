74d865a4b5704a3637a175049b69d79925c65c68
Rawan Tarek <115898539+rawwaanntarekk@users.noreply.github.com>
2024-10-24 14:51:11 +0300

    Part 01 Identity Module - Entities.

---
+++ LinkDev.Talabat.Core.Domain/Entities/Identity/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LinkDev.Talabat.Core.Domain.Entities.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public virtual required string DisplayName { get; set; }
+        public virtual Address? Address { get; set; }
+    }
+}
