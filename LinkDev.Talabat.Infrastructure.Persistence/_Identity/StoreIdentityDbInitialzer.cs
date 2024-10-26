ff88cbc0648eeb4aa45dcdfa81b307e45b5d71b1
Rawan Tarek <115898539+rawwaanntarekk@users.noreply.github.com>
2024-10-26 10:50:37 +0300

    Part 02- Add, Delete and update roles.

--- LinkDev.Talabat.Infrastructure.Persistence/_Identity/StoreIdentityDbInitialzer.cs
+++ LinkDev.Talabat.Infrastructure.Persistence/_Identity/StoreIdentityDbInitialzer.cs
@@ -6,10 +6,19 @@ using Microsoft.AspNetCore.Identity;
 namespace LinkDev.Talabat.Infrastructure.Persistence._Identity
 {
     internal class StoreIdentityDbInitialzer(StoreIdentityDbContext _dbContext,
-                                            UserManager<ApplicationUser> _userManager) : DbInitialzer(_dbContext), IStoreIdentityDbInitializer
+                                            UserManager<ApplicationUser> _userManager, RoleManager<IdentityRole> _roleManager) : DbInitialzer(_dbContext), IStoreIdentityDbInitializer
     {
         public override async Task SeedAsync()
         {
+            if (!await _roleManager.RoleExistsAsync("Admin"))
+            {
+                var adminRole = new IdentityRole();
+                adminRole.Name = "Admin";
+                await  _roleManager.CreateAsync(adminRole);
+                
+            }
+
+
             if (!_userManager.Users.Any())
             {
                 var user = new ApplicationUser
@@ -25,6 +34,10 @@ namespace LinkDev.Talabat.Infrastructure.Persistence._Identity
                 await _dbContext.Users.AddAsync(user);
                 await _dbContext.SaveChangesAsync();
             }
+
+
+
+
         }
     }
 }
