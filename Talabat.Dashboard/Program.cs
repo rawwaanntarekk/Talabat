c9af7c7d37e252b1a40411d3c6c829284aa3bea7
Rawan Tarek <115898539+rawwaanntarekk@users.noreply.github.com>
2024-10-26 09:09:42 +0300

    Part 01 - Adding Dashboard template.

---
+++ Talabat.Dashboard/Program.cs
@@ -0,0 +1,78 @@
+using LinkDev.Talabat.APIs.Extensions;
+using LinkDev.Talabat.APIs.Services;
+using LinkDev.Talabat.Core.Application;
+using LinkDev.Talabat.Core.Application.Abstraction.Contracts;
+using LinkDev.Talabat.Core.Domain.Entities.Identity;
+using LinkDev.Talabat.Infrastructure.Persistence;
+using LinkDev.Talabat.Infrastructure.Persistence._Identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace Talabat.Dashboard
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var webApplicationBuilder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            webApplicationBuilder.Services.AddControllersWithViews();
+
+            webApplicationBuilder.Services.AddScoped(typeof(ILoggedInUserService), typeof(LoggedInUserService)); 
+            webApplicationBuilder.Services.AddPersistenceServices(webApplicationBuilder.Configuration);
+
+            webApplicationBuilder.Services.AddIdentity<ApplicationUser, IdentityRole>(identityOptions =>
+            {
+                identityOptions.User.RequireUniqueEmail = true;
+                identityOptions.SignIn.RequireConfirmedPhoneNumber = true;
+                identityOptions.SignIn.RequireConfirmedAccount = true;
+                identityOptions.SignIn.RequireConfirmedEmail = true;
+
+
+                //identityOptions.Password.RequireNonAlphanumeric = true;
+                //identityOptions.Password.RequiredUniqueChars = 2;
+                //identityOptions.Password.RequiredLength = 6;
+                //identityOptions.Password.RequireDigit = true;
+                //identityOptions.Password.RequireLowercase = true;
+                //identityOptions.Password.RequireUppercase = true;
+
+
+                identityOptions.Lockout.AllowedForNewUsers = true;
+                identityOptions.Lockout.MaxFailedAccessAttempts = 10;
+                identityOptions.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+
+
+
+            })
+            .AddEntityFrameworkStores<StoreIdentityDbContext>();
+
+
+
+
+            var app = webApplicationBuilder.Build();
+            await app.InitializeDbAsync();
+
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
